19c9430550774d71ab82c1c53574c6c009f803b0
Angel Tovar <angeltovar308@gmail.com>
2023-02-27 09:29:27 -0600

    clase del 24-02-23 se reviso cajas dinamicas

--- app.py
+++ app.py
@@ -1,41 +1,66 @@
-from flask import Flask, render_template, request
-from forms import WordForm, TranslateForm
+# from flask import Flask, render_template, request,make_response,flash
+# from forms import WordForm, TranslateForm
+# from forms import LoginForm
+# import forms 
+# from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+# app = Flask(__name__)
+# app.config['SECRET_KEY']="esta es una clave encriptada"
+# csrf=CSRFProtect()
+# csrf.init_app(app)
+#-----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------LOGIN--------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+# @app.route("/cookie", methods = ['GET', 'POST'])
+# def cookie():
+    
+#     reg_user = forms.LoginForm(request.form)
+#     response = make_response(render_template('cookie.html', form = reg_user,))
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    word_form = WordForm()
-    translate_form = TranslateForm()
+#     if request.method == 'POST' and reg_user.validate():
+#         user = reg_user.username.data
+#         password = reg_user.password.data
+#         datos = user + '@' + password
+#         success_message = 'Bienvenido {}'.format(user)
+#         response.set_cookie('datos_usuario', datos)
+#         flash(success_message)
+#     return response
+#-----------------------------------------------------------------------------------------------------------------
+#--------------------------------------------DICCIONARIO DATOS----------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     word_form = WordForm()
+#     translate_form = TranslateForm()
 
-    if word_form.validate_on_submit():
-        with open('words.txt', 'a') as f:
-            f.write(f'{word_form.spanish_word.data.lower()}={word_form.english_word.data.lower()}\n')
-        return render_template('traductor.html', word_form=word_form, translate_form=translate_form, message='Guardado con éxito')
+#     if word_form.validate_on_submit():
+#         with open('words.txt', 'a') as f:
+#             f.write(f'{word_form.spanish_word.data.lower()}={word_form.english_word.data.lower()}\n')
+#         return render_template('traductor.html', word_form=word_form, translate_form=translate_form, message='Guardado con éxito')
 
-    if translate_form.validate_on_submit():
-        language = translate_form.language.data
-        with open('words.txt') as f:
-            words = dict(line.strip().lower().split('=') for line in f)
-            try:
-                if language == 'english':
-                    translation = words[translate_form.word.data.lower()]
-                else:
-                    translation = [key for key, value in words.items() if value == translate_form.word.data.lower()][0]
-                message = f'Traducción: {translation}'
-            except (KeyError, IndexError):
-                message = 'No se encuentra la traducción'
-        return render_template('traductor.html', word_form=word_form, translate_form=translate_form, message=message)
+#     if translate_form.validate_on_submit():
+#         language = translate_form.language.data
+#         with open('words.txt') as f:
+#             words = dict(line.strip().lower().split('=') for line in f)
+#             try:
+#                 if language == 'english':
+#                     translation = words[translate_form.word.data.lower()]
+#                 else:
+#                     translation = [key for key, value in words.items() if value == translate_form.word.data.lower()][0]
+#                 message = f'Traducción: {translation}'
+#             except (KeyError, IndexError):
+#                 message = 'No se encuentra la traducción'
+#         return render_template('traductor.html', word_form=word_form, translate_form=translate_form, message=message)
 
-    return render_template('traductor.html', word_form=word_form, translate_form=translate_form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#     return render_template('traductor.html', word_form=word_form, translate_form=translate_form)
 
 
 
+
+
+#-----------------------------------------------------------------------------------------------------------------
+#--------------------------------------------ALUMNOS--------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 
 
 
@@ -76,17 +101,19 @@ if __name__ == '__main__':
 
 
 
-# from flask import Flask, render_template, request
-# from flask_wtf.csrf import CSRFProtect
-# import forms  
-# from forms import MyData
+from flask import Flask, render_template, request
+from flask_wtf.csrf import CSRFProtect
+import forms  
+from forms import MyData
 
-# csrf = CSRFProtect()
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = "en un lugar de la mancha"
-# csrf.init_app(app)
+csrf = CSRFProtect()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "en un lugar de la mancha"
+csrf.init_app(app)
 
-
+#-----------------------------------------------------------------------------------------------------------------
+#--------------------------------------------ALUMNOS--------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 
 # @app.route("/formprueba")
 # def formprueba():
@@ -105,29 +132,34 @@ if __name__ == '__main__':
 
 #     return render_template('Alumnos.html', form=reg_alum,datos=datos)
 
-
-# @app.route('/cajasDinamicas', methods=['GET', 'POST'])
-# @csrf.exempt # Excluye la protección CSRF para las solicitudes GET
-# def cajasDinamicas():
-#     if request.method == 'GET':
-#         formulario = MyData()
-#         return render_template('index.html', form=formulario)
-#     else:
-#         formulario = MyData(request.form)
-#         return render_template('cajas-dinamicas.html', form=formulario)
+#-----------------------------------------------------------------------------------------------------------------
+#--------------------------------------------CAJAS DINAMICAS------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
+@app.route('/cajasDinamicas', methods=['GET', 'POST'])
+@csrf.exempt # Excluye la protección CSRF para las solicitudes GET
+def cajasDinamicas():
+    if request.method == 'GET':
+        formulario = MyData()
+        return render_template('cajas-dinamicas.html', form=formulario)
+    else:
+        formulario = MyData(request.form)
+        return render_template('cajas-dinamicas.html', form=formulario)
      
-# @app.route('/cajasDinamicas2', methods=['POST'])
-# @csrf.exempt # Excluye la protección CSRF para las solicitudes GET
-# def cajasDinamicas2():
-#     formulario = MyData(request.form)
-#     valores = [int(number) for number in formulario.numeros.data]
+@app.route('/cajasDinamicas2', methods=['POST'])
+@csrf.exempt # Excluye la protección CSRF para las solicitudes GET
+def cajasDinamicas2():
+    formulario = MyData(request.form)
+    valores = [int(number) for number in formulario.numeros.data]
     
-#     repetidos = []
+    repetidos = []
 
-#     for valor in set(valores):
-#         repeticiones = len([num for num in valores if num == valor])
-#         if repeticiones > 1:
-#             repetidos.append((valor, repeticiones))
+    for valor in set(valores):
+        repeticiones = len([num for num in valores if num == valor])
+        if repeticiones > 1:
+            repetidos.append((valor, repeticiones))
 
-#     return render_template('cajas-dinamicas2.html', valores=valores, minNum=min(valores), maxNum=max(valores), promedio=sum(valores) / len(valores), repetidos=repetidos)
+    return render_template('cajas-dinamicas2.html', valores=valores, minNum=min(valores), maxNum=max(valores), promedio=sum(valores) / len(valores), repetidos=repetidos)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
